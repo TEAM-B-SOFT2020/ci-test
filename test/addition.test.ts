362f7a2df6b4fc966136bd466ca25fa41a6cfb11
adamlass <33089759+adamlass@users.noreply.github.com>
2020-11-03 09:58:49 +0100

    changes to the failing test

--- test/addition.test.ts
+++ test/addition.test.ts
@@ -1,7 +1,7 @@
 test('two plus two is four', () => {
     expect(2 + 2).toBe(4);
-})
+});
 
 test("name is egon", () => {
-    expect("muhammed").toBe("egon")
-})
+    expect("muhammed").toEqual("egon");
+});
